c4afd92dc86af10788ae829afd8105409ea93a30
richard <richard@hicling.com>
2016-03-24 18:15:41 +0800

    Restore to workout page correctly after receiving a incoming message

--- project/source/ui.c
+++ project/source/ui.c
@@ -476,6 +476,14 @@ static void _perform_ui_with_a_finger_touch(UI_ANIMATION_CTX *u, I8U gesture)
 				
 				Y_SPRINTF("[UI] Landing to clock");
 			}
+			if (prev_index == UI_DISPLAY_SMART_INCOMING_MESSAGE) {
+				if (!cling.activity.b_workout_active)
+					cling.activity.workout_type = WORKOUT_NONE;
+				
+				if (cling.activity.workout_type > WORKOUT_NONE) {
+					u->frame_index = UI_DISPLAY_STOPWATCH_STOP;
+				}
+			}
 		}
 	} else if (gesture == TOUCH_FINGER_RIGHT) {
 	
@@ -508,7 +516,8 @@ static void _perform_ui_with_a_finger_touch(UI_ANIMATION_CTX *u, I8U gesture)
 		Y_SPRINTF("[UI] finger right: %d, %d", u->frame_index, u->b_detail_page);
 
 		// Update frame index
-		if (u->frame_index == UI_DISPLAY_SMART_INCOMING_CALL) {
+		if (u->frame_index == UI_DISPLAY_SMART_INCOMING_CALL)
+		{
 			if (!cling.activity.b_workout_active)
 				cling.activity.workout_type = WORKOUT_NONE;
 			
@@ -520,6 +529,7 @@ static void _perform_ui_with_a_finger_touch(UI_ANIMATION_CTX *u, I8U gesture)
 		} else {
 			u->frame_index = p_matrix[u->frame_index];
 		}
+
 		u->frame_next_idx = u->frame_index;
 	}
 }
